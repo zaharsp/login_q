db61f8f11119a229a93da68f6ac1bc1e5b131f85
zaharsp <ti.zahar.tec@gmail.com>
2024-11-25 21:49:16 -0300

    first commit

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php 
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "usuarios";
+    $port = 3306;
+    new PDO(mysql: host=$host; port=$port;dbname=". $dbname, $user, $pass);"
+?>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=devide-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="imagem/logo.png">
+    <link rel="stylesheet" href="CSS/estilo.css">
+    <title>Document</title>
+</head>
+<body>
+    <form actione="" class="form" method="POST">
+        <div class="card">
+            <div class="card-top">
+                <img src="imagem/icone.png.png" class="img-login">
+                <h2 class="title">Painel de controle</h2>
+                <p>Gerenciar seu negócio</p>
+            </div>
+
+            <div class="card-top">
+                <label for="">Nome Completo: </label><br>
+                <input type="text" name="nome" placeholder="Digite seu nome completo">
+            </div>
+            <br>
+            <div class="card-top">
+                <label for="">Email: </label><br>
+                <input type="email" name="email" placeholder="Digite seu email">                
+            </div>
+            <br>
+            <div class="card-top">
+                <label for="">Senha: </label><br>
+                <input type="password" name="senha" placeholder="Digite sua senha">
+            </div>
+        </div>
+    </form>
+</body>
+</html>
